161310391913bf1dad11552bbf154113c81aafb2
Chenqijia1234 <86812582@qq.com>
2023-02-26 16:09:32 +0800

    modified:   src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,27 @@
 """
 Python Console Library
-Copyright (C) 2022  鱼翔浅底,吴宇航
-Distributed under GPL 3.0 License
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+MIT License
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Copyright (c) 2023 鱼翔浅底(ChenQJ),吴宇航
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-Distributed under GPL license
-See copy at https://opensource.org/licenses/GPL-3.0
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 感谢吴宇航原创的代码着色功能！！！ 原作链接：
 ‘https://code.xueersi.com/home/project/detail?lang=code&pid=39312324&version=offline&form=python&langType=python’
@@ -435,7 +439,7 @@ class ChoiceScreen(object):
             title: str,
             choices: list,
             _console: Console,
-            info: Optional,
+            info: Optional[str],
             _current_option_color=ConsoleColor.Green,
             _enter_key=ConsoleKeys["Enter"]
     ) -> None:
